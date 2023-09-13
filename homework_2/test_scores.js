54200f7b78c7ef08e7318821edb67e2b48e37f1e
pd-ualr <pdeano@ualr.edu>
2023-09-13 17:48:53 +0000

    homework #2 why did i overthink this lol

---
+++ homework_2/test_scores.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const scores = [];
+
+// use do-while loop to get the scores from the user
+let score = 0;
+let highest_score = 0;
+
+// prompt to ask the user about score
+do {
+    score = parseInt(
+        prompt("Enter a test score, or enter -1 to end scores", -1));
+
+    if (score >= 0 && score <= 100) {
+        scores[scores.length] = score;
+    }
+    else if (score != -1){
+        alert("Score must by a valid number from 0 through 100");
+    }
+}
+while(score != -1);
+
+// what happens if the score is not -1
+if (scores.length > 0) {
+    // use a for-in loop to add each score to total, and display score
+    let total = 0;
+    for (let i in scores) {
+        total = total + scores[i];
+        document.write(`<p>Score ${parseInt(i) + 1}: ${scores[i]}</p>`);
+    }
+    // for of loop 
+    for (let val of scores) {
+        if (val > highest_score) {
+          highest_score = val;
+        }
+    }
+    
+    // calculate and display the average and highest score
+    const average = parseInt(total/scores.length);
+    document.write(`<p>Average score is ${average}</p>`);
+    document.write(`<p>Highest score is ${highest_score}</p>`);
+}
